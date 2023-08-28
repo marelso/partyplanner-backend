0c859952e6ec22091767576a19775fe9bad5f29c
Marcelo Klein <marcelo.klein@townsq.com.br>
2023-08-28 17:46:29 -0300

    Include new methods to account repository

--- src/main/java/com/marelso/partyplanner/repository/AccountRepository.java
+++ src/main/java/com/marelso/partyplanner/repository/AccountRepository.java
@@ -1,4 +1,14 @@
 package com.marelso.partyplanner.repository;
 
-public interface AccountRepository {
+import com.marelso.partyplanner.domain.Account;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface AccountRepository extends JpaRepository<Account, Integer> {
+    List<Account> findAllByDeletedFalse();
+    Optional<Account> findByUsername(String username);
 }
