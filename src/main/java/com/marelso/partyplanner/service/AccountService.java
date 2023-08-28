9846a068a8a233a0e1d2f6022ee45bd231f521f7
Marcelo Klein <marcelo.klein@townsq.com.br>
2023-08-28 17:49:16 -0300

    Updating account service to search approperly for the username

--- src/main/java/com/marelso/partyplanner/service/AccountService.java
+++ src/main/java/com/marelso/partyplanner/service/AccountService.java
@@ -1,14 +1,34 @@
 package com.marelso.partyplanner.service;
 
-import org.springframework.security.core.userdetails.UserDetails;
+import com.marelso.partyplanner.domain.Account;
+import com.marelso.partyplanner.repository.AccountRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+
 @Service
+@RequiredArgsConstructor
 public class AccountService implements UserDetailsService {
+    private final AccountRepository repository;
+
+    public List<Account> findAll() {
+        return this.repository.findAllByDeletedFalse();
+    }
+
+    private Account findUser(String username) {
+        return searchUsername(username)
+                .orElseThrow(() -> new RuntimeException("There is no account with id: ${id}"));
+    }
+
+    private Optional<Account> searchUsername(String username) {
+        return this.repository.findByUsername(username);
+    }
+
     @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return null;
+    public Account loadUserByUsername(String username) {
+        return findUser(username);
     }
 }
