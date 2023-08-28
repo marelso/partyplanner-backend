8011631f435ae3f6f0457378382223625cbaf8fe
Marcelo Klein <marcelo.klein@townsq.com.br>
2023-08-28 06:24:40 -0300

    Initial create

---
+++ src/main/java/com/marelso/partyplanner/SwaggerConfig.java
@@ -0,0 +1,45 @@
+package com.marelso.partyplanner;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RestController;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .groupName("Party Planner API")
+                .select()
+                .apis(RequestHandlerSelectors.withClassAnnotation(RestController.class))
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(info());
+    }
+
+    private ApiInfo info() {
+        return new ApiInfoBuilder()
+                .title("Party Planner API Documentation")
+                .description("API documentation for the Party Planner application.")
+                .version("1.0.0")
+                .termsOfServiceUrl("https://example.com/terms")
+                .contact(getContactInfo())
+                .build();
+    }
+
+    private Contact getContactInfo() {
+        return new Contact(
+                "Your Name",
+                "https://example.com/contact",
+                "contact@example.com"
+        );
+    }
+}
+
