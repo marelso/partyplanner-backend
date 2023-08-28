2f1c1bc594226586137e830fa150ba1371409ccb
Marcelo Klein <marcelo.klein@townsq.com.br>
2023-08-28 17:44:16 -0300

    Include new properties to Account entity

--- src/main/java/com/marelso/partyplanner/domain/Account.java
+++ src/main/java/com/marelso/partyplanner/domain/Account.java
@@ -1,43 +1,51 @@
 package com.marelso.partyplanner.domain;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
+import javax.persistence.*;
+import java.util.ArrayList;
 import java.util.Collection;
 
+@Getter
+@Setter
+@Entity(name = "accounts")
 public class Account implements UserDetails {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    @Column(nullable = false, unique = true)
+    private String username;
+    @Column(nullable = false, unique = true)
+    private String email;
+    @Column(nullable = false)
+    private String password;
+    private String name;
+    private String lastName;
+    private String profilePicture;
+    private PermissionType permissionType;
+    private boolean deleted;
+
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return null;
+        return new ArrayList<>();
     }
-
-    @Override
-    public String getPassword() {
-        return null;
-    }
-
-    @Override
-    public String getUsername() {
-        return null;
-    }
-
     @Override
     public boolean isAccountNonExpired() {
-        return false;
+        return !this.deleted;
     }
-
     @Override
     public boolean isAccountNonLocked() {
-        return false;
+        return !this.deleted;
     }
-
     @Override
     public boolean isCredentialsNonExpired() {
-        return false;
+        return !this.deleted;
     }
-
     @Override
     public boolean isEnabled() {
-        return false;
+        return !this.deleted;
     }
 }
