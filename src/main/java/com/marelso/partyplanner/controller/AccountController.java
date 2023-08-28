6951eed8f8f15b438404ac60d6c2a4a33e4b9248
Marcelo Klein <marcelo.klein@townsq.com.br>
2023-08-28 17:51:53 -0300

    Include 2 endpoints to handle user searching

--- src/main/java/com/marelso/partyplanner/controller/AccountController.java
+++ src/main/java/com/marelso/partyplanner/controller/AccountController.java
@@ -1,4 +1,26 @@
 package com.marelso.partyplanner.controller;
 
+import com.marelso.partyplanner.domain.Account;
+import com.marelso.partyplanner.service.AccountService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController("accounts")
+@RequiredArgsConstructor
 public class AccountController {
+    private final AccountService service;
+
+    @GetMapping()
+    public List<Account> get() {
+        return this.service.findAll();
+    }
+
+    @GetMapping("/{username}")
+    public Account get(@PathVariable String username) {
+        return this.service.loadUserByUsername(username);
+    }
 }
