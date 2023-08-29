9980cbd85d0f64a159c7e73e844856ffef43f607
Marcelo Klein <marcelo.klein@townsq.com.br>
2023-08-29 07:56:53 -0300

    The put endpoint will not use AccountDto anymore as the deleted and permission type arent updated setting at that endpoint

---
+++ src/main/java/com/marelso/partyplanner/dto/AccountPropertiesDto.java
@@ -0,0 +1,15 @@
+package com.marelso.partyplanner.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class AccountPropertiesDto {
+    private String username;
+    private String email;
+    private String name;
+    private String lastName;
+    private String bio;
+    private String profilePicture;
+}
