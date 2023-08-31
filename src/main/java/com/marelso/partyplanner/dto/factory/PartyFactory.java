5fb08f1774e1c38be2c40ff472c9fd6e7d732264
Marcelo Klein <marcelo.klein@townsq.com.br>
2023-08-31 17:00:38 -0300

    Created new method to party factory that will return an entity

--- src/main/java/com/marelso/partyplanner/dto/factory/PartyFactory.java
+++ src/main/java/com/marelso/partyplanner/dto/factory/PartyFactory.java
@@ -1,2 +1,22 @@
-package com.marelso.partyplanner.dto.factory;public class PartyFactory {
+package com.marelso.partyplanner.dto.factory;
+
+import com.marelso.partyplanner.domain.Account;
+import com.marelso.partyplanner.domain.Party;
+import com.marelso.partyplanner.dto.PartyCreateDto;
+import org.springframework.stereotype.Component;
+
+@Component
+public class PartyFactory {
+    public Party from(PartyCreateDto dto, Account createdBy) {
+        var entity = new Party();
+
+        entity.setName(dto.getName());
+        entity.setDescription(dto.getDescription());
+        entity.setRecurrence(dto.getRecurrence());
+        entity.setCreatedBy(createdBy);
+        entity.setStart(dto.getStart());
+        entity.setEnd(dto.getEnd());
+
+        return entity;
+    }
 }
