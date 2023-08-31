83493a4bffdbad36dd4266324a58e12921dcf88e
Marcelo Klein <marcelo.klein@townsq.com.br>
2023-08-31 17:01:03 -0300

    Create new method to party service that will handle party creation

--- src/main/java/com/marelso/partyplanner/service/PartyService.java
+++ src/main/java/com/marelso/partyplanner/service/PartyService.java
@@ -1,5 +1,8 @@
 package com.marelso.partyplanner.service;
 
+import com.marelso.partyplanner.domain.Party;
+import com.marelso.partyplanner.dto.PartyCreateDto;
+import com.marelso.partyplanner.dto.factory.PartyFactory;
 import com.marelso.partyplanner.repository.PartyRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -7,5 +10,12 @@ import org.springframework.stereotype.Service;
 @Service
 @RequiredArgsConstructor
 public class PartyService {
+    private final AccountService accountService;
     private final PartyRepository repository;
+    private final PartyFactory factory;
+
+    public Party create(PartyCreateDto request) {
+        var account = accountService.findUser(request.getCreatedBy());
+        return repository.save(factory.from(request, account));
+    }
 }
