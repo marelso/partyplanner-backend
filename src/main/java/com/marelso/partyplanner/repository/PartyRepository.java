e9dfa3102dc235c3d87e5ee23a77913d1f5fa03c
Marcelo Klein <marcelo.klein@townsq.com.br>
2023-08-31 16:46:22 -0300

    Update party repo to have listing method by the given account

--- src/main/java/com/marelso/partyplanner/repository/PartyRepository.java
+++ src/main/java/com/marelso/partyplanner/repository/PartyRepository.java
@@ -4,6 +4,9 @@ import com.marelso.partyplanner.domain.Party;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface PartyRepository extends JpaRepository<Party, Integer> {
+    List<Party> findAllByCreatedBy_IdOrderByStartDesc(Integer accountId);
 }
