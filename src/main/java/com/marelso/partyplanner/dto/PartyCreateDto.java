35fee7024671e59498896801edad29487538871f
Marcelo Klein <marcelo.klein@townsq.com.br>
2023-08-31 17:00:17 -0300

    Updated partyCreateDto to have username instead of the userId

--- src/main/java/com/marelso/partyplanner/dto/PartyCreateDto.java
+++ src/main/java/com/marelso/partyplanner/dto/PartyCreateDto.java
@@ -1,13 +1,17 @@
 package com.marelso.partyplanner.dto;
 
 import com.marelso.partyplanner.domain.Recurrence;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.time.OffsetDateTime;
 
+@Getter
+@Setter
 public class PartyCreateDto {
     private String name;
     private String description;
-    private Integer createdBy;
+    private String createdBy;
     private Recurrence recurrence;
     private OffsetDateTime start;
     private OffsetDateTime end;
