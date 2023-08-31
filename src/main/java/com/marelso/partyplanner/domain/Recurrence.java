3f077d2c2794184df284601107de0b9f93320684
Marcelo Klein <marcelo.klein@townsq.com.br>
2023-08-31 16:25:27 -0300

    Created new entity to handle party recurrence

---
+++ src/main/java/com/marelso/partyplanner/domain/Recurrence.java
@@ -0,0 +1,18 @@
+package com.marelso.partyplanner.domain;
+
+public enum Recurrence {
+    ONETIME("One-time party"),
+    WEEKLY("Weekly"),
+    MONTHLY("Monthly"),
+    YEARLY("Yearly");
+
+    private final String recurrence;
+
+    Recurrence(String recurrence) {
+        this.recurrence = recurrence;
+    }
+
+    public String getRecurrence() {
+        return recurrence;
+    }
+}
