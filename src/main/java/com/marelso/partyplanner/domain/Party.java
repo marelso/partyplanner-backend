b84b7ff6e29f97c62e731fb2298d0aa7f5547ad7
Marcelo Klein <marcelo.klein@townsq.com.br>
2023-08-31 16:33:15 -0300

    Create new entity to handle parties

--- src/main/java/com/marelso/partyplanner/domain/Party.java
+++ src/main/java/com/marelso/partyplanner/domain/Party.java
@@ -1,4 +1,21 @@
 package com.marelso.partyplanner.domain;
 
+import com.marelso.partyplanner.dto.AccountDto;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.time.OffsetDateTime;
+
 public class Party {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String name;
+    private String description;
+    private Recurrence recurrence;
+    private String bannerURL;
+    private Account createdBy;
+    private OffsetDateTime start;
+    private OffsetDateTime end;
 }
