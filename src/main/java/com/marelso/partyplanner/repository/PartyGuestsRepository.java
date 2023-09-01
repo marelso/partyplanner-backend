2ac7983577247b6f436b04474dcfb744dce2fa5a
Marcelo Klein <marcelo.klein@townsq.com.br>
2023-09-01 07:30:20 -0300

    Include new method to find all guests from the party

--- src/main/java/com/marelso/partyplanner/repository/PartyGuestsRepository.java
+++ src/main/java/com/marelso/partyplanner/repository/PartyGuestsRepository.java
@@ -4,6 +4,9 @@ import com.marelso.partyplanner.domain.PartyGuest;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface PartyGuestsRepository extends JpaRepository<PartyGuest, Integer> {
+    List<PartyGuest> findAllByPartyId(Integer partyId);
 }
