329ed87b4f1f3e36191a1f812544e2a0d988aeb8
Marcelo Klein <marcelo.klein@townsq.com.br>
2023-09-01 01:40:49 -0300

    Updating factory to do not receive an account object and have an from party list method

--- src/main/java/com/marelso/partyplanner/dto/factory/PartyFactory.java
+++ src/main/java/com/marelso/partyplanner/dto/factory/PartyFactory.java
@@ -3,20 +3,41 @@ package com.marelso.partyplanner.dto.factory;
 import com.marelso.partyplanner.domain.Account;
 import com.marelso.partyplanner.domain.Party;
 import com.marelso.partyplanner.dto.PartyCreateDto;
+import com.marelso.partyplanner.dto.PartyDto;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Component
 public class PartyFactory {
-    public Party from(PartyCreateDto dto, Account createdBy) {
+    public Party from(PartyCreateDto dto, Integer accountId) {
         var entity = new Party();
 
         entity.setName(dto.getName());
         entity.setDescription(dto.getDescription());
         entity.setRecurrence(dto.getRecurrence());
-        entity.setCreatedBy(createdBy);
+        entity.setAccountId(accountId);
         entity.setStart(dto.getStart());
         entity.setEnd(dto.getEnd());
 
         return entity;
+    }
+
+    public PartyDto from(Party entity, String username) {
+        var dto = new PartyDto();
+
+        dto.setName(entity.getName());
+        dto.setDescription(entity.getDescription());
+        dto.setRecurrence(entity.getRecurrence());
+        dto.setCreatedBy(username);
+        dto.setStart(entity.getStart());
+        dto.setEnd(entity.getEnd());
+
+        return dto;
+    }
+
+    public List<PartyDto> from(List<Party> parties, String username) {
+        return parties.stream().map(party -> from(party, username)).collect(Collectors.toList());
     }
 }
