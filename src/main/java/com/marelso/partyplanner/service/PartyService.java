8969613e716fc3a1a08e6dba9cca998afb22e9b7
Marcelo Klein <marcelo.klein@townsq.com.br>
2023-09-01 02:39:16 -0300

    Created creating and listing methods to party service

--- src/main/java/com/marelso/partyplanner/service/PartyService.java
+++ src/main/java/com/marelso/partyplanner/service/PartyService.java
@@ -1,11 +1,13 @@
 package com.marelso.partyplanner.service;
 
-import com.marelso.partyplanner.domain.Party;
 import com.marelso.partyplanner.dto.PartyCreateDto;
+import com.marelso.partyplanner.dto.PartyDto;
 import com.marelso.partyplanner.dto.factory.PartyFactory;
 import com.marelso.partyplanner.repository.PartyRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -14,8 +16,16 @@ public class PartyService {
     private final PartyRepository repository;
     private final PartyFactory factory;
 
-    public Party create(PartyCreateDto request) {
-        var account = accountService.findUser(request.getCreatedBy());
-        return repository.save(factory.from(request, account));
+    public List<PartyDto> list(String username) {
+        var account = accountService.findUser(username);
+
+        return factory.from(this.repository.findAllByAccountId(account.getId()), account.getUsername());
+    }
+
+    public PartyDto create(PartyCreateDto request, String username) {
+        var account = accountService.findUser(username);
+        var party = repository.save(factory.from(request, account.getId()));
+
+        return factory.from(party, account.getUsername());
     }
 }
