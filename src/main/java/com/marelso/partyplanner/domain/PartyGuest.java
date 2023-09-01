0f2b68f6b1446c9213820d611545ed0fc9a1a796
Marcelo Klein <marcelo.klein@townsq.com.br>
2023-09-01 06:55:48 -0300

    Including getters and setters

--- src/main/java/com/marelso/partyplanner/domain/PartyGuest.java
+++ src/main/java/com/marelso/partyplanner/domain/PartyGuest.java
@@ -1,10 +1,15 @@
 package com.marelso.partyplanner.domain;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+@Getter
+@Setter
 @Entity(name = "party_guests")
 public class PartyGuest {
     @Id
