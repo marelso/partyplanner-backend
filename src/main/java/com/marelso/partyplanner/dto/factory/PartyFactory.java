49cb81abbbdf0f4efa03e347c40ffcfc2f066198
Marcelo Klein <marcelo.klein@townsq.com.br>
2023-09-01 06:08:53 -0300

    Created new method in party factory that will handle party updates. And including new property reference

--- src/main/java/com/marelso/partyplanner/dto/factory/PartyFactory.java
+++ src/main/java/com/marelso/partyplanner/dto/factory/PartyFactory.java
@@ -4,6 +4,7 @@ import com.marelso.partyplanner.domain.Account;
 import com.marelso.partyplanner.domain.Party;
 import com.marelso.partyplanner.dto.PartyCreateDto;
 import com.marelso.partyplanner.dto.PartyDto;
+import com.marelso.partyplanner.dto.PartyUpdateDto;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
@@ -27,6 +28,7 @@ public class PartyFactory {
     public PartyDto from(Party entity, String username) {
         var dto = new PartyDto();
 
+        dto.setReference(entity.getId());
         dto.setName(entity.getName());
         dto.setDescription(entity.getDescription());
         dto.setRecurrence(entity.getRecurrence());
@@ -39,5 +41,15 @@ public class PartyFactory {
 
     public List<PartyDto> from(List<Party> parties, String username) {
         return parties.stream().map(party -> from(party, username)).collect(Collectors.toList());
+    }
+
+    public Party from(Party party, PartyUpdateDto request) {
+        party.setName(request.getName());
+        party.setDescription(request.getDescription());
+        party.setRecurrence(request.getRecurrence());
+        party.setStartDate(request.getStart());
+        party.setEndDate(request.getEnd());
+
+        return party;
     }
 }
