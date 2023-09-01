7d197f44b580e6e1a45a47efb1960e9f4e9d696f
Marcelo Klein <marcelo.klein@townsq.com.br>
2023-09-01 06:56:07 -0300

    Including new method to invite users to given party

--- src/main/java/com/marelso/partyplanner/service/PartyGuestsService.java
+++ src/main/java/com/marelso/partyplanner/service/PartyGuestsService.java
@@ -1,5 +1,6 @@
 package com.marelso.partyplanner.service;
 
+import com.marelso.partyplanner.domain.PartyGuest;
 import com.marelso.partyplanner.repository.PartyGuestsRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -8,4 +9,13 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class PartyGuestsService {
     private final PartyGuestsRepository repository;
+
+    public void inviteUserToParty(Integer account, Integer party) {
+        var guest = new PartyGuest();
+
+        guest.setPartyId(party);
+        guest.setAccountId(account);
+
+        repository.save(guest);
+    }
 }
