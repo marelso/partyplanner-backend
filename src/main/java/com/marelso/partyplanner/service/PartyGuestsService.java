769c0a57c03d778612b816100e9a426629c8c714
Marcelo Klein <marcelo.klein@townsq.com.br>
2023-09-01 06:51:01 -0300

    Including repository reference to party guestss service

--- src/main/java/com/marelso/partyplanner/service/PartyGuestsService.java
+++ src/main/java/com/marelso/partyplanner/service/PartyGuestsService.java
@@ -1,8 +1,11 @@
 package com.marelso.partyplanner.service;
 
+import com.marelso.partyplanner.repository.PartyGuestsRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class PartyGuestsService {
-
+    private final PartyGuestsRepository repository;
 }
