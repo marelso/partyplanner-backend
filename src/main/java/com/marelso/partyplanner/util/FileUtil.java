75e56903f31b2f750adec1e3d094a8377bd08989
Marcelo Klein <marcelo.klein@townsq.com.br>
2023-09-02 10:40:53 -0300

    Removing unused imports

--- src/main/java/com/marelso/partyplanner/util/FileUtil.java
+++ src/main/java/com/marelso/partyplanner/util/FileUtil.java
@@ -1,7 +1,5 @@
 package com.marelso.partyplanner.util;
 
-import lombok.experimental.UtilityClass;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
