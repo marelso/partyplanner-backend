99d6ce2d889d1ba58bb0ab1423b2d5826a6baeb8
Marcelo Klein <marcelo.klein@townsq.com.br>
2023-09-18 19:17:45 -0300

    Including auth service to gift controller

--- src/main/java/com/marelso/partyplanner/controller/GiftController.java
+++ src/main/java/com/marelso/partyplanner/controller/GiftController.java
@@ -1,7 +1,9 @@
 package com.marelso.partyplanner.controller;
 
+import com.marelso.partyplanner.domain.PermissionType;
 import com.marelso.partyplanner.dto.CreationGiftDto;
 import com.marelso.partyplanner.dto.GiftDto;
+import com.marelso.partyplanner.service.AuthService;
 import com.marelso.partyplanner.service.GiftService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
@@ -11,12 +13,15 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/gifts")
 public class GiftController {
     private final GiftService service;
+    private final AuthService authService;
 
     @PostMapping
     public GiftDto post(
+            @RequestHeader("Authorization") String token,
             @RequestParam Integer partyId,
             @RequestBody CreationGiftDto request
     ) {
+        authService.authorize(token, PermissionType.USER);
         return service.create(request, partyId);
     }
 }
--- src/main/java/com/marelso/partyplanner/service/GiftService.java
+++ src/main/java/com/marelso/partyplanner/service/GiftService.java
@@ -9,6 +9,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -26,7 +28,7 @@ public class GiftService {
     }
 
     public List<GiftDto> list(Integer partyId) {
-
+        return null;
     }
 
     public void removeFromParty(Integer giftId, Integer partyId) {
