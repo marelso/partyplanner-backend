899a3003180d8ca99a9b89cbabd91f61c9bd17fe
Marcelo Klein <marcelo.klein@townsq.com.br>
2023-09-18 18:45:00 -0300

    Include method to delete association by gift id

--- src/main/java/com/marelso/partyplanner/repository/PartyGiftRepository.java
+++ src/main/java/com/marelso/partyplanner/repository/PartyGiftRepository.java
@@ -5,4 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PartyGiftRepository extends JpaRepository<PartyGift, Integer> {
     Boolean existsByPartyIdAndGiftId(Integer partyId, Integer giftId);
+    void deleteAllByGiftId(Integer giftId);
 }
