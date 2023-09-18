32bc55a59e2ef7a14f74f01cdd9558fe25cf4f7e
Marcelo Klein <marcelo.klein@townsq.com.br>
2023-09-18 13:47:22 -0300

    Include new entity to apply relation between party and gifts

--- src/main/java/com/marelso/partyplanner/domain/PartyGifts.java
+++ src/main/java/com/marelso/partyplanner/domain/PartyGifts.java
@@ -1,2 +1,20 @@
-package com.marelso.partyplanner.domain;public class PartyGifts {
+package com.marelso.partyplanner.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Getter
+@Setter
+@Entity(name = "party_gifts")
+public class PartyGifts {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private Integer partyId;
+    private Integer giftId;
 }
--- src/main/java/com/marelso/partyplanner/repository/PartyGiftRepository.java
+++ src/main/java/com/marelso/partyplanner/repository/PartyGiftRepository.java
@@ -1,2 +1,9 @@
-package com.marelso.partyplanner.repository;public interface PartyGiftRepository {
+package com.marelso.partyplanner.repository;
+
+import com.marelso.partyplanner.domain.PartyGifts;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface PartyGiftRepository extends JpaRepository<PartyGifts, Integer> {
 }
