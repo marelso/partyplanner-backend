f95bd4d7d8309e156dcd8de32af4be3d93feb9e3
Marcelo Klein <marcelo.klein@townsq.com.br>
2023-09-18 13:33:22 -0300

    Created gift service with repository

--- src/main/java/com/marelso/partyplanner/service/GiftService.java
+++ src/main/java/com/marelso/partyplanner/service/GiftService.java
@@ -1,2 +1,17 @@
-package com.marelso.partyplanner.service;public class GiftService {
+package com.marelso.partyplanner.service;
+
+import com.marelso.partyplanner.domain.Gift;
+import com.marelso.partyplanner.dto.CreationGiftDto;
+import com.marelso.partyplanner.repository.GiftRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class GiftService {
+    private final GiftRepository repository;
+
+    public Gift save(CreationGiftDto request) {
+
+    }
 }
