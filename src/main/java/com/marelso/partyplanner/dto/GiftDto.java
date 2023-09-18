9d489493681c825e0a091877029ed62b47eeace0
Marcelo Klein <marcelo.klein@townsq.com.br>
2023-09-18 19:02:30 -0300

    Including new Dto to handle gift data transfer

---
+++ src/main/java/com/marelso/partyplanner/dto/GiftDto.java
@@ -0,0 +1,13 @@
+package com.marelso.partyplanner.dto;
+
+import com.marelso.partyplanner.domain.Link;
+
+import java.util.List;
+
+public class GiftDto {
+    private Integer reference;
+    private String title;
+    private String description;
+    private String image;
+    private List<Link> links;
+}
