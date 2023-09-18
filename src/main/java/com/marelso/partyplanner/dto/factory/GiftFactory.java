12fe3e52a77b9fc76dfdb7cbde25b1ba52c65cfb
Marcelo Klein <marcelo.klein@townsq.com.br>
2023-09-18 13:35:53 -0300

    Created factory with from CreationGiftDto

--- src/main/java/com/marelso/partyplanner/dto/factory/GiftFactory.java
+++ src/main/java/com/marelso/partyplanner/dto/factory/GiftFactory.java
@@ -1,2 +1,19 @@
-package com.marelso.partyplanner.dto.factory;public class GiftFactory {
+package com.marelso.partyplanner.dto.factory;
+
+import com.marelso.partyplanner.domain.Gift;
+import com.marelso.partyplanner.dto.CreationGiftDto;
+import org.springframework.stereotype.Component;
+
+@Component
+public class GiftFactory {
+    public Gift from(CreationGiftDto dto) {
+        var entity = new Gift();
+
+        entity.setTitle(dto.getTitle());
+        entity.setDescription(dto.getDescription());
+        entity.setImage(dto.getImage());
+        entity.setLinks(dto.getLinks());
+
+        return entity;
+    }
 }
