976b20b496244873564ddd3a9b17360a99899029
Marcelo Klein <marcelo.klein@townsq.com.br>
2023-09-18 13:32:56 -0300

    Populating dto with necessary fields

--- src/main/java/com/marelso/partyplanner/dto/CreationGiftDto.java
+++ src/main/java/com/marelso/partyplanner/dto/CreationGiftDto.java
@@ -1,2 +1,16 @@
-package com.marelso.partyplanner.dto;public class CreationGiftDto {
+package com.marelso.partyplanner.dto;
+
+import com.marelso.partyplanner.domain.Link;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+public class CreationGiftDto {
+    private String title;
+    private String description;
+    private String image;
+    private List<Link> links;
 }
