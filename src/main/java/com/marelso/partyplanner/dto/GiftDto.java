79ba49acd265e47a054b3b2595da55f147f53056
Marcelo Klein <marcelo.klein@townsq.com.br>
2023-09-18 19:06:18 -0300

    Including getters and setters

--- src/main/java/com/marelso/partyplanner/dto/GiftDto.java
+++ src/main/java/com/marelso/partyplanner/dto/GiftDto.java
@@ -1,9 +1,13 @@
 package com.marelso.partyplanner.dto;
 
 import com.marelso.partyplanner.domain.Link;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.util.List;
 
+@Getter
+@Setter
 public class GiftDto {
     private Integer reference;
     private String title;
