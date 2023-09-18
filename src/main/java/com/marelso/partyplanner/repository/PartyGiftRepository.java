32f9397a5ae11dca8591931f3c081489eef655ab
Marcelo Klein <marcelo.klein@townsq.com.br>
2023-09-18 18:56:58 -0300

    Fixing method to return optional

--- src/main/java/com/marelso/partyplanner/repository/PartyGiftRepository.java
+++ src/main/java/com/marelso/partyplanner/repository/PartyGiftRepository.java
@@ -3,7 +3,10 @@ package com.marelso.partyplanner.repository;
 import com.marelso.partyplanner.domain.PartyGift;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface PartyGiftRepository extends JpaRepository<PartyGift, Integer> {
     Boolean existsByPartyIdAndGiftId(Integer partyId, Integer giftId);
+    Optional<PartyGift> findByPartyIdAndGiftId(Integer partyId, Integer giftId);
     void deleteAllByGiftId(Integer giftId);
 }
