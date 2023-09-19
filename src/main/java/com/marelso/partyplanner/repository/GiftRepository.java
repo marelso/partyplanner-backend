13f7798cdd96ca0e4931c5fb1b3ac4ebcc6df6fb
Marcelo Klein <marcelo.klein@townsq.com.br>
2023-09-19 17:27:22 -0300

    Include new method to return pages of Gift by given id list

--- src/main/java/com/marelso/partyplanner/repository/GiftRepository.java
+++ src/main/java/com/marelso/partyplanner/repository/GiftRepository.java
@@ -1,9 +1,14 @@
 package com.marelso.partyplanner.repository;
 
 import com.marelso.partyplanner.domain.Gift;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface GiftRepository extends JpaRepository<Gift, Integer> {
+    Page<Gift> findAllByIdIn(List<Integer> id, Pageable pageable);
 }
