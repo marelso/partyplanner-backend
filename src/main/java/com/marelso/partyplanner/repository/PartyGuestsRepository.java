175b273d1f00980b293a77a8086fad955e34c6bc
Marcelo Klein <marcelo.klein@townsq.com.br>
2023-09-19 16:43:20 -0300

    Created new method to return parties ids for given account

--- src/main/java/com/marelso/partyplanner/repository/PartyGuestsRepository.java
+++ src/main/java/com/marelso/partyplanner/repository/PartyGuestsRepository.java
@@ -12,7 +12,8 @@ import java.util.Optional;
 @Repository
 public interface PartyGuestsRepository extends JpaRepository<PartyGuest, Integer> {
     List<PartyGuest> findAllByPartyId(Integer partyId);
-
+    @Query(value = "select pg.party_id from party_guests pg where pg.account_id = :accountId", nativeQuery = true)
+    List<Integer> findPartyIdByAccountId(@Param("accountId") Integer accountId);
     @Query(value = "select * from party_guests guest where guest.account_id = :accountId and guest.party_id = :partyId", nativeQuery = true)
     Optional<PartyGuest> findByPartyIdAndAccountId(@Param("accountId") Integer accountId, @Param("partyId") Integer partyId);
 }
