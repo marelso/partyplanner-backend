78983b079e3ebc857cbd25bc1e2778401e38fae6
Marcelo Klein <marcelo.klein@townsq.com.br>
2023-09-19 14:55:27 -0300

    Passing page info to repository

--- src/main/java/com/marelso/partyplanner/repository/PartyRepository.java
+++ src/main/java/com/marelso/partyplanner/repository/PartyRepository.java
@@ -1,6 +1,8 @@
 package com.marelso.partyplanner.repository;
 
 import com.marelso.partyplanner.domain.Party;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -10,12 +12,12 @@ import java.util.List;
 
 @Repository
 public interface PartyRepository extends JpaRepository<Party, Integer> {
-    List<Party> findAllByAccountIdOrderByStartDateAsc(Integer accountId);
+    Page<Party> findAllByAccountIdOrderByStartDateAsc(Integer accountId, Pageable pageable);
 
     @Query(value = "SELECT * FROM parties p " +
             "WHERE p.account_id = :accountId " +
             "AND p.start_date >= now() and p.start_date <= now()+ interval '6 day' " +
             "AND p.end_date > now() " +
             "ORDER BY p.start_date ASC " , nativeQuery = true)
-    List<Party> upcomingParties(@Param("accountId") Integer accountId);
+    Page<Party> upcomingParties(@Param("accountId") Integer accountId, Pageable pageable);
 }
