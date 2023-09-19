ba886249be51f3e8978bb9907f25f8293fd238e5
Marcelo Klein <marcelo.klein@townsq.com.br>
2023-09-18 21:01:06 -0300

    Doing use of querys to return the correct ids at repository level. The previous method was returning all entities

--- src/main/java/com/marelso/partyplanner/repository/PartyGiftRepository.java
+++ src/main/java/com/marelso/partyplanner/repository/PartyGiftRepository.java
@@ -2,10 +2,15 @@ package com.marelso.partyplanner.repository;
 
 import com.marelso.partyplanner.domain.PartyGift;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface PartyGiftRepository extends JpaRepository<PartyGift, Integer> {
+    @Query(value = "select gift_id from party_gifts pg where pg.party_id = :partyId", nativeQuery = true)
+    List<Integer> findGiftsIdsByPartyId(@Param("partyId") Integer partyId);
     Boolean existsByPartyIdAndGiftId(Integer partyId, Integer giftId);
     Optional<PartyGift> findByPartyIdAndGiftId(Integer partyId, Integer giftId);
     void deleteAllByGiftId(Integer giftId);
